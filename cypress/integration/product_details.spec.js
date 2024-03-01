af4faca74ccc16da2ad0ef7c31c952b8868ba9f4
Tom Spencer <thomas.spencer2034@gmail.com>
2024-03-01 09:26:39 -0500

    Add product_details test

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,9 +1,9 @@
-describe('home-tests', () => {
+describe('product-details-tests', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:3000');
   });
   it("There is products on the page", () => {
-    cy.get(".products article").should("be.visible");
+    cy.get("[alt='Cliff Collard']").click()
+    cy.contains("Cliff Collard")
   });
 })
-
